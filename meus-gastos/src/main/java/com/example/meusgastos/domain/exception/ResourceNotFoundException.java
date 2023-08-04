f361c7a9ffad5eddfc5b23ec854fca4b10d21654
willian <128076027+willlg@users.noreply.github.com>
2023-08-04 13:32:32 -0300

    Delete meus-gastos directory

--- meus-gastos/src/main/java/com/example/meusgastos/domain/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.meusgastos.domain.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensagem){
-        super(mensagem);
-    }
-    
-}
